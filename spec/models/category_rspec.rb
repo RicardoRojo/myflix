b6f96db5956570e8406fff0b9acecce51e300305
Ricardo Rojo <ricardorojo@netscape.net>
2015-05-06 18:55:30 -0400

    added rspec tests for video and category models

---
+++ spec/models/category_rspec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Category do
+  it "should create record and save it" do
+    category = Category.new(name: "drama")
+    category.save
+    expect(Category.first.name).to eq("drama")
+  end
+  it "should create record and verify relation with Video table" do
+    category = Category.new(name: "drama", videos: [Video.create(title: "Death on the nile")])
+    category.save
+    expect(Category.first.videos.first.title).to eq("Death on the nile")
+  end
+end
--- spec/models/video_rspec.rb
+++ spec/models/video_rspec.rb
@@ -9,4 +9,13 @@ describe Video do
     video.save
     expect(Video.first.title).to eq("Death on the nile")
   end
+  it "creates record and verify relation" do
+    video = Video.new(title: "Death on the nile",
+      description: "As Hercule Poirot enjoys a luxurious cruise down the Nile, a newlywed heiress is found murdered on board.",
+      small_cover_url: "tmp/monk.jpg",
+      large_cover_url: "tmp/monk_large.jpg",
+      category: Category.create(name: "drama"))
+    video.save
+    expect(Video.first.category.name).to eq("drama")
+  end
 end
