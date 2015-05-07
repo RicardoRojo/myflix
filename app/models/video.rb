7dae7e2a0fcad396c8e67049e705800351d94ba9
Ricardo Rojo <ricardorojo@netscape.net>
2015-05-07 15:10:51 -0400

    added search_by_title method with TDD

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,9 @@ class Video < ActiveRecord::Base
   belongs_to :category
   validates :title, presence: true
   validates :description, presence: true
+
+  def self.search_by_title(string)
+    return [] if string.empty?
+    where("title LIKE ?", "%#{string}%").order(created_at: :asc)
+  end
 end
