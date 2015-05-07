488afbcbc0a580d0cfec6edb58134396e6d5a0f7
Ricardo Rojo <ricardorojo@netscape.net>
2015-05-06 20:42:44 -0400

    test refactor to include should matchers

--- spec/models/category_rspec.rb
+++ spec/models/category_rspec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe Category do
-  it "should create record and save it" do
-    category = Category.new(name: "drama")
-    category.save
-    expect(Category.first.name).to eq("drama")
-  end
-  it "should create record and verify relation with Video table" do
-    category = Category.new(name: "drama", videos: [Video.create(title: "Death on the nile", description: "a good movie")])
-    category.save
-    expect(Category.first.videos.first.title).to eq("Death on the nile")
-  end
+  it {should have_many(:videos)}
+  it {should validate_uniqueness_of(:name)}
 end
--- spec/models/video_rspec.rb
+++ spec/models/video_rspec.rb
@@ -1,29 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves data" do
-    video = Video.new(title: "Death on the nile",
-      description: "As Hercule Poirot enjoys a luxurious cruise down the Nile, a newlywed heiress is found murdered on board.",
-      small_cover_url: "tmp/monk.jpg",
-      large_cover_url: "tmp/monk_large.jpg")
-    video.save
-    expect(Video.first.title).to eq("Death on the nile")
-  end
-  it "creates record and verify relation" do
-    video = Video.new(title: "Death on the nile",
-      description: "As Hercule Poirot enjoys a luxurious cruise down the Nile, a newlywed heiress is found murdered on board.",
-      small_cover_url: "tmp/monk.jpg",
-      large_cover_url: "tmp/monk_large.jpg",
-      category: Category.create(name: "drama"))
-    video.save
-    expect(Video.first.category.name).to eq("drama")
-  end
-  it "validates presence of title" do
-    video = Video.create(description: "description without a title video")
-    expect(Video.count).to eq(0)
-  end
-  it "validates presence of description" do
-    video = Video.create(title: "Death on the nile")
-    expect(Video.count).to eq(0)
-  end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should belong_to(:category) }
 end
