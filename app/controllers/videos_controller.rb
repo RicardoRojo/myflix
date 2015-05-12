f2b191ae71be1efdcbcc2376890567f0c9381f2e
Ricardo Rojo <ricardorojo@netscape.net>
2015-05-12 07:01:45 -0400

    added controller tests for show and search

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,6 @@
 class VideosController < ApplicationController
-  before_action :require_user
+  before_action :require_user, except: [:search]
+  before_action :redirect_to_sign_in, only: [:search]
   
   def index
     @categories = Category.includes(:videos)
@@ -12,4 +13,5 @@ class VideosController < ApplicationController
   def search
     @videos = Video.search_by_title(params[:video_title])
   end
+
 end
---
+++ spec/models/video_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Video do
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+  it { should belong_to(:category) }
+
+  describe "#search_by_title" do
+
+    it "should return an empty array if the search is an empty string" do
+      star_trek = Video.create(title: "star trek", description: "Live long and prosper")
+      stargate = Video.create(title: "stargate", description: "Travel to the stars")
+      expect(Video.search_by_title("")).to eq([])
+    end
+
+    it "should return an empty array if no records found" do
+      star_trek = Video.create(title: "star trek", description: "Live long and prosper")
+      stargate = Video.create(title: "stargate", description: "Travel to the stars")
+      expect(Video.search_by_title("matrix")).to eq([])
+    end
+
+    it "should return an array of one element if the query returns one and only one result" do
+      star_trek = Video.create(title: "star trek", description: "Live long and prosper")
+      stargate = Video.create(title: "stargate", description: "Travel to the stars")
+      expect(Video.search_by_title("stargate")).to eq([stargate])
+    end
+
+    it "should return an array of coincident records if no exact match" do
+      star_trek = Video.create(title: "star trek", description: "Live long and prosper")
+      stargate = Video.create(title: "stargate", description: "Travel to the stars")
+      expect(Video.search_by_title("star")).to eq([star_trek, stargate])
+    end
+    
+    it "should return an array of records ordered by created_at if multiple records are found " do
+      star_trek = Video.create(title: "star trek", description: "Live long and prosper")
+      stargate = Video.create(title: "stargate", description: "Travel to the stars", created_at: 1.day.ago)
+      expect(Video.search_by_title("star")).to eq([stargate,star_trek])
+    end
+  end
+end
