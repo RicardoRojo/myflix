0142cb6119f48436848200a1adb19b1678ca0dc5
Ricardo Rojo <ricardorojo@netscape.net>
2015-05-18 19:59:36 -0400

    added 'add to my queue'

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -4,4 +4,21 @@ class QueueItemsController < ApplicationController
   def index
     @queue_items = current_user.queue_items
   end
+
+  def create
+    video = Video.find(params[:video_id])
+
+    add_item_to_queue(video) unless item_is_already_queued?(video)
+    redirect_to my_queue_path
+  end
+
+  private
+
+  def add_item_to_queue(video)
+    QueueItem.create!(video_id: params[:video_id], user: current_user, position: current_user.queue_items.count + 1)
+  end
+
+  def item_is_already_queued?(video)
+    !!QueueItem.where("video_id = ? and user_id = ?", params[:video_id], current_user.id).first
+  end
 end
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -1,17 +1,71 @@
 require 'spec_helper'
 
 describe QueueItemsController do
-  it "has a @queueitems variable" do
-    current_user = Fabricate(:user)
-    session[:user_id] = current_user.id
-    queueitem = Fabricate(:queue_item, user: current_user)
-    queueitem2 = Fabricate(:queue_item, user: current_user)
-    get :index
-    expect(assigns(:queue_items)).to eq([queueitem,queueitem2])
+  describe "GET index" do
+    context "with user logged in" do
+
+      it "has a @queueitems variable" do
+        current_user = Fabricate(:user)
+        session[:user_id] = current_user.id
+        queueitem = Fabricate(:queue_item, user: current_user)
+        queueitem2 = Fabricate(:queue_item, user: current_user)
+        get :index
+        expect(assigns(:queue_items)).to eq([queueitem,queueitem2])
+      end
+    end
+
+    context "with no user logged in " do
+
+      it "redirects to root path" do
+        get :index
+        expect(response).to redirect_to root_path
+      end
+    end
   end
 
-  it "redirects to root path if no user" do
-    get :index
-    expect(response).to redirect_to root_path
+  describe "POST create" do
+    context "with user logged in" do
+
+      let(:user) {Fabricate(:user)}
+      let(:monk) {Fabricate(:video, title: "Monk")}
+      
+      before do
+        session[:user_id] = user.id
+      end
+
+      it "redirects to my queue page" do
+        post :create, video_id: monk.id
+        expect(response).to redirect_to my_queue_path
+      end
+
+      it "creates the queue item" do
+        post :create, video_id: monk.id
+        expect(QueueItem.count).to eq(1)
+      end
+
+      it "does not create a record if the video is already in the queue" do
+        queue_item = Fabricate(:queue_item, user: user, video: monk)
+        post :create, video_id: monk.id
+        expect(QueueItem.count).to be(1)
+      end
+
+      it "shows the queued item in the last position" do
+        futurama = Fabricate(:video, title: "Futurama")
+        queue_item = Fabricate(:queue_item, user: user, video: monk)
+        post :create, video_id: futurama.id
+        last_queued_item = QueueItem.where("user_id = ? and video_id = ?", user.id, futurama.id).first
+        expect(last_queued_item.position).to eq(2)
+      end
+    end
+
+    context "with no user logged in" do
+
+      it "redirects to the root page" do
+        video = Fabricate(:video)
+        post :create, video_id: video.id
+        expect(response).to redirect_to root_path
+      end
+    end
   end
+
 end
