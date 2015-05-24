0ac12e00272daf99c4164b84657450d15dbd1faa
Ricardo Rojo <ricardorojo@netscape.net>
2015-05-24 05:25:56 -0400

    added corrections from tom tomecek

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def options_for_select_with_stars(default_value)
+    options_for_select([5,4,3,2,1].map {|rating| [pluralize(rating, "Star"), rating]}, default_value)
+  end
 end
